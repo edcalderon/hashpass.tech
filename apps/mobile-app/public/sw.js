19771c7c7d09b150f1dd868b3beae443bb353a82
edcalderon <edwardca12@hotmail.com>
2026-07-27 17:54:31 -0500

    chore: release v1.8.271

--- apps/mobile-app/public/sw.js
+++ apps/mobile-app/public/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.8.270';
+const APP_VERSION = '1.8.271';
 const STATIC_CACHE = `hashpass-static-v${APP_VERSION}`;
 const PAGE_CACHE = `hashpass-pages-v${APP_VERSION}`;
 const OFFLINE_URL = '/offline.html';
