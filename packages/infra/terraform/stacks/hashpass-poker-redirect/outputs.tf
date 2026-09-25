562e7ce179f6050327b20493cbdf8d8a419c04e2
edcalderon <edwardca12@hotmail.com>
2026-09-25 13:20:25 -0500

    ✨ feat: add Poker Room redirects

---
+++ packages/infra/terraform/stacks/hashpass-poker-redirect/outputs.tf
@@ -0,0 +1,9 @@
+output "hash_poker_room_distribution_id" {
+  description = "CloudFront distribution serving pkkr.hashpass.tech"
+  value       = module.hash_poker_room.cloudfront_distribution_id
+}
+
+output "pkrr_io_distribution_id" {
+  description = "CloudFront distribution serving pkrr.io.hashpass.tech"
+  value       = module.pkrr_io.cloudfront_distribution_id
+}
---
+++ packages/infra/terraform/stacks/hashpass-poker-redirect/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region for the redirect distributions' S3 origin resources"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "hashpass_zone_name" {
+  description = "Existing public Route 53 zone containing the hashpass.tech subdomains"
+  type        = string
+  default     = "hashpass.tech"
+}
+
+variable "hash_poker_room_domain" {
+  description = "Short public Hash Poker Room hostname"
+  type        = string
+  default     = "pkkr.hashpass.tech"
+}
+
+variable "pkrr_io_domain" {
+  description = "Hashpass-owned PKRR alias that canonicalizes to pkrr.io"
+  type        = string
+  default     = "pkrr.io.hashpass.tech"
+}
+
+variable "tags" {
+  description = "Tags applied to every managed redirect resource"
+  type        = map(string)
+  default     = {}
+}
